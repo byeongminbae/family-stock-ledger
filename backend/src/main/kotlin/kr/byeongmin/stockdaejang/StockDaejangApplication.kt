8175bbbd2884ae3fa435bf1cacfea0e997bac41d
byeongminbae <151404574+byeongminbae@users.noreply.github.com>
2026-08-14 00:39:29 +0900

    백/프론트 분리

---
+++ backend/src/main/kotlin/kr/byeongmin/stockdaejang/StockDaejangApplication.kt
@@ -0,0 +1,11 @@
+package kr.byeongmin.stockdaejang
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StockDaejangApplication
+
+fun main(args: Array<String>) {
+    runApplication<StockDaejangApplication>(*args)
+}
---
+++ backend/src/test/kotlin/kr/byeongmin/stockdaejang/StockDaejangApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.byeongmin.stockdaejang
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StockDaejangApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
